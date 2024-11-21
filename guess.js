1e29254728594a688af5ec821c18b74f703d1e5d
Gideon Simon <simonsamuel195@gmail.com>
2024-11-21 23:07:25 +0100

    Number guessing game

--- guess.js
+++ guess.js
@@ -1,6 +1,6 @@
 // document.querySelector(".message").style.backgroundColor = "#ff5733"
 
-let hiddenNumber =Math.trunc(Math.random()  * 20) + 1
+let hiddenNumber = Math.trunc(Math.random()  * 20) + 1
 let score = 20;
 
 console.log(hiddenNumber )
@@ -11,6 +11,25 @@ const displayMessage =(message) => {
 }
 
 // document.querySelector(".number").textContent = hiddenNumber
+
+const againButton = document.querySelector(".again");
+againButton.addEventListener("click", () => {
+    score = 20;
+    hiddenNumber = Math.trunc(Math.random() * 20) + 1;
+
+    let message = 'Start guessing...';
+    let number = "?";
+    let guessInput = document.querySelector(".guess");
+    
+    document.querySelector(".score").textContent = score;
+    document.querySelector(".message").textContent = message;
+    document.querySelector(".number").textContent = number;
+    document.querySelector("body").style.backgroundColor = "#222";
+    
+    if (guessInput) {
+        guessInput.value = "";
+    }
+});
 
 document.querySelector(".check").addEventListener("click", () => {
     const guess = Number(document.querySelector(".guess").value)
@@ -25,7 +44,7 @@ document.querySelector(".check").addEventListener("click", () => {
     } else if (guess !== hiddenNumber ) {
         if (score > 1 ) {
             displayMessage(guess > hiddenNumber ? "Your guess is too high" : "Your guess is too low")
-            score-2
+            score--
             document.querySelector(".score").textContent = score
 
         } else {
